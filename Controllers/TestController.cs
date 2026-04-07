e23e79533ee77a2af0508fac3b0b1104eab13013
Andy Chen <angey920518@gmail.com>
2026-04-07 11:17:23 +0800

    chore : 新增 Test 檔案

---
+++ Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitFlow.Practice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+    }
+}
